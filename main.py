3655c4d39d8a2ab9e08da96da5b25f239f4fe27c
CHEN <qilingz1995@gmail.com>
2024-10-18 13:23:22 +0800

    creat a 'main.py' file

---
+++ main.py
@@ -0,0 +1,16 @@
+import SpiderGui
+import Pic_Down_241012
+
+def main():
+    SpiderGui.main()
+    SpiderGui.
+'''
+    filename =  # input("Enter file name: ")
+    creat_file(filename)
+    url = 'https://spacemiss.com/'  # + input("Enter URL: ")
+    down_pic(url, filename)
+    Pic_Down_241012.
+'''
+
+if __name__ == '__main__':
+    main()
